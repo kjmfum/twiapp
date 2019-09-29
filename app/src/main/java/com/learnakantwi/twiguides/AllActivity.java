1754f0bd15d19fa6db25a0f382ba65779e125749
kjmfum <kjmfum@yahoo.co.uk>
2019-09-29 21:07:53 +0000

    commit 2

--- app/src/main/java/com/learnakantwi/twiguides/AllActivity.java
+++ app/src/main/java/com/learnakantwi/twiguides/AllActivity.java
@@ -1,10 +1,17 @@
 package com.learnakantwi.twiguides;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,7 +26,14 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -33,23 +47,130 @@ public class AllActivity extends AppCompatActivity {
     static ArrayList<All> allArrayList;
     ListView allListView;
     AdView mAdView;
+    StorageReference storageReference;
+    MediaPlayer playFromDevice;
+    MediaPlayer mp1;
 
-    public void generate() {
 
 
 
-        String english1;
-        english1 = allArrayList.get(1).toString();
-        Log.i("Got It", english1);
+
+    public void playFromFileOrDownload(final String filename){
+        File myFile = new File("/storage/emulated/0/Android/data/com.learnakantwi.twiguides/files/Music/"+filename+ ".m4a");
+        if (myFile.exists()){
+
+            try {
+                if (playFromDevice != null){
+                    playFromDevice.stop();
+                    playFromDevice.reset();
+                    playFromDevice.release();
+                }
+                playFromDevice = new MediaPlayer();
+
+                playFromDevice.setDataSource(myFile.toString());
+                playFromDevice.prepareAsync();
+                playFromDevice.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        mp.start();
+                    }
+                });
+                Toast.makeText(this, "From Device", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+
+
+            final StorageReference musicRef = storageReference.child("/AllTwi/" + filename + ".m4a");
+            musicRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    String url = uri.toString();
+                    playFromFirebase(musicRef);
+                   // Toast.makeText(getApplicationContext(), "Got IT", Toast.LENGTH_SHORT).show();
+                    downloadFile(getApplicationContext(), filename, ".m4a", url);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+    }
+    public void playFromFirebase(StorageReference musicRef) {
+
+        if (Build.VERSION.SDK_INT > 22) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+
+        try {
+            final File localFile = File.createTempFile("aduonu", "m4a");
+
+            if (localFile != null) {
+                musicRef.getFile(localFile)
+                        .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                                if (mp1 != null){
+                                    mp1.stop();
+                                    mp1.reset();
+                                    mp1.release();
+                                }
+                                mp1 = new MediaPlayer();
+                                try {
+                                    Toast.makeText(AllActivity.this, "Yes Yes", Toast.LENGTH_SHORT).show();
+                                    mp1.setDataSource(AllActivity.this, Uri.fromFile(localFile));
+                                    mp1.prepareAsync();
+                                    mp1.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                                        @Override
+                                        public void onPrepared(MediaPlayer mp) {
+                                            mp.start();
+                                        }
+                                    });
+                                } catch (IOException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle failed download
+                        // ...
+                    }
+                });
+            } else {
+                Toast.makeText(this, "File was not created", Toast.LENGTH_SHORT).show();
+            }
+
+        }catch(IOException ex){
+                ex.printStackTrace();
+            }
+
+        }
+
+    public void downloadFile(Context context, String filename, String fileExtension, String url){
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setVisibleInDownloadsUi(false);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        //   request.setDestinationInExternalPublicDir(Environment.DIRECTORY_MUSIC+File.separator+"LearnTwi1", filename+fileExtension);
+        request.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_MUSIC, filename+fileExtension);
+        //request.setDestinationInExternalPublicDir(Environment.DIRECTORY_MUSIC+File.separator+"LearnTwi1", filename+fileExtension);
+        downloadManager.enqueue(request);
+
     }
 
     public void log2(View view) {
 
-generate();
-
         int idview = view.getId();
 
-        TextView blabla = (TextView) view.findViewById(idview);
+        TextView blabla = view.findViewById(idview);
         String a = (String) blabla.getText();
 
         Toast.makeText(this, a, Toast.LENGTH_SHORT).show();
@@ -59,7 +180,7 @@ generate();
 
         int idview= view.getId();
 
-        TextView blabla = (TextView) view.findViewById(idview);
+        TextView blabla = view.findViewById(idview);
         String a = (String) blabla.getText();
         Toast.makeText(this, a, Toast.LENGTH_SHORT).show();
 
@@ -84,7 +205,10 @@ generate();
             b= b.replace("?","");
         }
 
-        int resourceId = getResources().getIdentifier(b, "raw", "com.learnakantwi.twiguides");
+
+        playFromFileOrDownload(b);
+
+       /* int resourceId = getResources().getIdentifier(b, "raw", "com.learnakantwi.twiguides");
 
 
         final MediaPlayer player = MediaPlayer.create(this, resourceId);
@@ -96,6 +220,8 @@ generate();
                 player.release();
             }
         });
+
+        */
 
     }
     @Override
@@ -182,6 +308,8 @@ generate();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all);
 
+
+
     /* MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
@@ -194,6 +322,8 @@ generate();
 
         //allArrayList = new ArrayList<>();
         allListView = findViewById(R.id.allListView);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         /*
         //Animals
